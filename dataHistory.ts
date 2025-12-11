5f5135245ef1868c6cf71ea7fe0f2b8abd34d77e
thanhkira16 <hthanh16092004@gmail.com>
2025-12-11 13:17:44 +0700

    api test

---
+++ dataHistory.ts
@@ -0,0 +1,38 @@
+const practiceHistory = [{
+    id: 1,
+    part: 1,
+    part_id: 1,
+    created_at: "",
+    overall_score: 6.0,
+    details: {},
+    general_suggestions: [] 
+}]
+
+const examHistory = [{
+    id: 1,
+    created_at: "",
+    overall_score: 6.0,
+    data: [
+    {
+        part: 1,
+        part_id: 1,
+        overall_score: 6.0,
+        details: {},
+        general_suggestions: []
+    },
+    {
+        part: 2,
+        part_id: 2,
+        overall_score: 6.0,
+        details: {},
+        general_suggestions: []
+    },
+    {
+        part: 3,
+        part_id: 3,
+        overall_score: 6.0,
+        details: {},
+        general_suggestions: []
+    }
+]
+}];
